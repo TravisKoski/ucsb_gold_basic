59a921b0eadafee291a0074ed9a67f2b49fb4fa2
Travis Koski <tmkoski69@gmail.com>
2022-04-22 11:45:37 -0700

    Cchanged the nroll route to a put, and changed the link to a button

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   resources :students
   resources :courses
   get "/students/:student_id/courses", to: "courses#index", as: "view_courses_as_student" #Intermediary step for student enrolling in courses
-  get "/students/:student_id/courses/:course_id/enroll", to: "students#enroll", as: "enroll_in_course"
+  put "/students/:student_id/courses/:course_id/enroll", to: "students#enroll", as: "enroll_in_course"
   get "/students/:student_id/view_courses", to: "students#view_courses", as: "view_classes"
   #route to view the associated students of a course
-  get "/courses/:id/view_students", to: "courses#view_students", as: "course_student_roster"
+  put "/courses/:id/view_students", to: "courses#view_students", as: "course_student_roster"
+  patch "/students/:student_id/courses/:course_id/drop", to: "students#drop_class", as: "drop_class"
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
